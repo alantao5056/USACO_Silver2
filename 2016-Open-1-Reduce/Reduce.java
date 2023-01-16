172ffa98dac0cc4470dd9f8c0aff36366a915f4a
alantao5056 <alantao5056@outlook.com>
2023-01-16 15:13:53 -0500

    finished reduce

--- 2016-Open-1-Reduce/Reduce.java
+++ 2016-Open-1-Reduce/Reduce.java
@@ -9,14 +9,6 @@ import java.util.*;
 public class Reduce {
   static StreamTokenizer st;
   static int N;
-  static int minX;
-  static Set<Cow> minXSet;
-  static int maxX;
-  static Set<Cow> maxXSet;
-  static int minY;
-  static Set<Cow> minYSet;
-  static int maxY;
-  static Set<Cow> maxYSet;
 
   public static void main(String[] args) throws Exception {
     // read input
@@ -28,126 +20,60 @@ public class Reduce {
     N = nextInt();
     
     // solve
-    Cow[] cows = new Cow[N];
+    List<Coord> coordsX = new ArrayList<>();
+    List<Coord> coordsY = new ArrayList<>();
     for (int i = 0; i < N; i++) {
-      cows[i] = new Cow(nextInt(), nextInt());
+      Coord cur = new Coord(nextInt(), nextInt());
+      coordsX.add(cur);
+      coordsY.add(cur);
     }
 
-    get(cows, null, null);
+    Collections.sort(coordsX, (Coord a, Coord b) -> a.x-b.x);
+    Collections.sort(coordsY, (Coord a, Coord b) -> a.y-b.y);
 
-    int init = (maxX - minX) * (maxY - minY);
+    int result = Integer.MAX_VALUE;
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 4; k++) {
+          for (int l = 0; l < 4; l++) {
+            int minX = coordsX.get(i).x;
+            int maxX = coordsX.get(N-j-1).x;
+            int minY = coordsY.get(k).y;
+            int maxY = coordsY.get(N-l-1).y;
+            if (minX > maxX || minY > maxY) continue;
 
-    Set<Cow> remove = new HashSet<>();
-    if (minXSet.size() <= 3) {
-      remove.addAll(minXSet);
-    }
-
-    if (maxXSet.size() <= 3) {
-      remove.addAll(maxXSet);
-    }
-
-    if (minYSet.size() <= 3) {
-      remove.addAll(minYSet);
-    }
-
-    if (maxYSet.size() <= 3) {
-      remove.addAll(maxYSet);
-    }
-
-    List<Cow> removeList = new ArrayList<>();
-    removeList.addAll(remove);
-
-    if (removeList.size() > 2) {
-      int result = Integer.MAX_VALUE;
-      for (int i = 0; i < removeList.size(); i++) {
-        for (int j = i+1; j < removeList.size(); j++) {
-          for (int k = j+1; k < removeList.size(); k++) {
-            int minX_ = Integer.MAX_VALUE;
-            int maxX_ = -1;
-            int minY_ = Integer.MAX_VALUE;
-            int maxY_ = -1;
-
-            for (int l = 0; l < N; l++) {
-              if (cows[l] != removeList.get(i) && cows[l] != removeList.get(j) && cows[l] != removeList.get(k)) {
-                minX = Math.min(minX, cows[l].x);
-                maxX = Math.max(maxX, cows[l].x);
-                minY = Math.min(minY, cows[l].y);
-                maxY = Math.max(maxY, cows[l].y);
+            int count = 0;
+            for (int m = 0; count <= 3 && m < N; m++) {
+              if (coordsX.get(m).x < minX || coordsX.get(m).x > maxX || coordsX.get(m).y < minY || coordsX.get(m).y > maxY) {
+                count++;
               }
             }
 
-            result = Math.min(result, (maxX_-minX_) * (maxY_-minY_));
+            if (count <= 3) {
+              result = Math.min(result, (maxX-minX) * (maxY-minY));
+            }
           }
         }
       }
-      pw.println(result);
-    } else {
-      if (removeList.size() == 0) {
-        pw.println(init);
-      } else if (removeList.size() == 1) {
-        get(cows, removeList.get(0), null);
-        if (maxXSet.size() <= 2) {
-
-        }
-      }
     }
+
+    pw.println(result);
 
     br.close();
     pw.close();
   }
 
-  private static void get(Cow[] arr, Cow n1, Cow n2) {
-    minX = Integer.MAX_VALUE;
-    minXSet = new HashSet<>();
-    maxX = -1;
-    maxXSet = new HashSet<>();
-    minY = Integer.MAX_VALUE;
-    minYSet = new HashSet<>();
-    maxY = -1;
-    maxYSet = new HashSet<>();
-
-    for (int i = 0; i < N; i++) {
-      if (arr[i] == n1 || arr[i] == n2) continue;
-      if (arr[i].x < minX) {
-        minX = arr[i].x;
-        minXSet = new HashSet<>();
-        minXSet.add(arr[i]);
-      } else if (arr[i].x == minX) {
-        minXSet.add(arr[i]);
-      }
-
-      if (arr[i].x > maxX) {
-        maxX = arr[i].x;
-        maxXSet = new HashSet<>();
-        maxXSet.add(arr[i]);
-      } else if (arr[i].x == maxX) {
-        maxXSet.add(arr[i]);
-      }
-
-      if (arr[i].y < minY) {
-        minY = arr[i].y;
-        minYSet = new HashSet<>();
-        minYSet.add(arr[i]);
-      } else if (arr[i].y == minY) {
-        minYSet.add(arr[i]);
-      }
-
-      if (arr[i].y > maxY) {
-        maxY = arr[i].y;
-        maxYSet = new HashSet<>();
-        maxYSet.add(arr[i]);
-      } else if (arr[i].y == maxY) {
-        maxYSet.add(arr[i]);
-      }
-    }
-  }
-
-  private static class Cow {
+  private static class Coord {
     int x;
     int y;
-    public Cow(int x, int y) {
+    public Coord(int x, int y) {
       this.x = x;
       this.y = y;
+    }
+
+    @Override
+    public String toString() {
+      return x + " " + y;
     }
   }
 
